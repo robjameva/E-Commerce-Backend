e1b2c06235f144a3be2e5b36a2ba5609d52a4306
Robert Evanik <rob.evanik@gmail.com>
2022-01-23 22:15:47 -0500

    products  routes functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
@@ -32,7 +32,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ],
     where: {
